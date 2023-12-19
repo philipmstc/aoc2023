2e3727bdb8881205e7a4e0c32f26c96f255eb7e4
philipmstc <philipmstc@gmail.com>
2023-12-18 22:36:20 -0330

    D11P1 success

--- cosmic-expansion-day11/Part1.scala
+++ cosmic-expansion-day11/Part1.scala
@@ -1,7 +1,24 @@
 import scala.io.Source._
+import scala.math._
 
 object Part1 {
-  def main(args: Array[String]) {
-    fromFile(args(0))
+  def main(args: Array[String]) = {
+    val board: Array[String] = fromFile(args(0)).getLines.toArray
+    
+    var galaxies: Array[(Int, Int)] = Array()
+    for (y <- 0 until board.size) {
+      for (x <- 0 until board(y).size) {
+        if (board(y)(x) == '#') { 
+          galaxies = galaxies :+ (y, x)
+        }
+      }
+    }
+    var sum = 0
+    for (i <- 0 until galaxies.size-1) {
+      for (j <- (i+1) until galaxies.size) { 
+        sum += (abs(galaxies(i)(0) - galaxies(j)(0)) + abs(galaxies(i)(1) - galaxies(j)(1))).toInt
+      }
+    }
+    println(sum)
   }
 }
